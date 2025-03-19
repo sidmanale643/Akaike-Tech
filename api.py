7a60db9d6df89d22aa395c25065fcccdfc3f2fda
Sidhant Manale <sidmanale643@gmail.com>
2025-03-19 14:11:22 +0530

    Refactor `api.py` for improved readability and PEP8 compliance

--- api.py
+++ api.py
@@ -1,24 +1,44 @@
 from fastapi import FastAPI
-from utils import fetch_from_web, analyze_sentiment, generate_comparative_sentiment, generate_final_report, get_summaries_by_sentiment, translate , text_to_speech
+from utils import (
+    fetch_from_web,
+    analyze_sentiment,
+    generate_comparative_sentiment,
+    generate_final_report,
+    get_summaries_by_sentiment,
+    translate,
+    text_to_speech,
+)
 
 app = FastAPI()
 
+
 @app.get("/home")
-def main(company_name: str , model_provider : str):
+def main(company_name: str, model_provider: str):
     web_results = fetch_from_web(company_name)
-    
-    if 'sources' not in web_results:
+
+    if "sources" not in web_results:
         return {"error": "No sources found."}
 
-    sentiment_output = [analyze_sentiment(article , model_provider) for article in web_results['sources'][:5]]
-    
+    sentiment_output = [
+        analyze_sentiment(article, model_provider)
+        for article in web_results["sources"][:5]
+    ]
+
     comparative_sentiment = generate_comparative_sentiment(sentiment_output)
 
-    positive_summary, negative_summary, neutral_summary = get_summaries_by_sentiment(sentiment_output)
+    positive_summary, negative_summary, neutral_summary = get_summaries_by_sentiment(
+        sentiment_output
+    )
 
-    final_report = generate_final_report(positive_summary, negative_summary, neutral_summary, comparative_sentiment , model_provider)
-    hindi_translation = translate(final_report , model_provider)
+    final_report = generate_final_report(
+        positive_summary,
+        negative_summary,
+        neutral_summary,
+        comparative_sentiment,
+        model_provider,
+    )
 
+    hindi_translation = translate(final_report, model_provider)
     audio_path = text_to_speech(hindi_translation)
 
     return {
@@ -27,5 +47,5 @@ def main(company_name: str , model_provider : str):
         "comparative_sentiment": comparative_sentiment,
         "final_report": final_report,
         "hindi_translation": hindi_translation,
-        "audio_url": audio_path
+        "audio_url": audio_path,
     }
