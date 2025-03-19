028beee3c1a6a58394cfccea21e247ace3cd5521
Sidhant Manale <sidmanale643@gmail.com>
2025-03-19 14:09:55 +0530

    PEP8 compliance

--- app.py
+++ app.py
@@ -4,27 +4,30 @@ import requests
 st.title("Company Sentiment Analyzer")
 
 company_name = st.text_input("Enter Company Name", "Tesla")
-model_provider = st.selectbox("Model Provider" , options= [ "Ollama" , "Groq"])
+model_provider = st.selectbox("Model Provider", options=["Ollama", "Groq"])
 
 if st.button("Fetch Sentiment Data"):
-    api_url = f"http://localhost:8000/home?company_name={company_name}&model_provider={model_provider}"
+    api_url = (
+        f"http://localhost:8000/home?"
+        f"company_name={company_name}&model_provider={model_provider}"
+    )
 
     try:
         response = requests.get(api_url)
-        response.raise_for_status()  
+        response.raise_for_status()
 
         data = response.json()
 
         st.subheader("Company Name")
         st.write(data.get("company_name"))
-        
+
         st.subheader("Final Report")
         st.write(data.get("final_report"))
-        
+
         st.subheader("🔊 Audio Output")
         audio_file = "output.mp3"
         if audio_file:
             st.audio(audio_file)
-            
+
     except requests.exceptions.RequestException as e:
         st.error(f"Error fetching data: {e}")
